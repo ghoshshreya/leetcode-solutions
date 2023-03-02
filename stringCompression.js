6ce1e052359d953208a148a0a12179946b81a290
ghoshshreya <shrghosh08@gmail.com>
2023-03-02 11:52:07 +0530

    1 file modified

--- stringCompression.js
+++ stringCompression.js
@@ -1,20 +1,26 @@
-function stringCompression(arr) {
+var compress = function (chars) {
   let s = '';
-  let n = arr.length;
-  for (let x = 0; x < n; x++) {
-    if (s.length === 0) {
-      s = s + arr[x];
-      s = s + '1';
-    } //second last element of is same as curr element
-    else if (s[s.length - 2] === arr[x]) {
-      let count = Number(s[s.length - 1]) + 1;
-      s = s.substring(0, s.length - 1) + count;
-    } else {
-      s = s + arr[x];
-      s = s + '1';
+  let n = chars.length;
+  if (n > 0 && n > 1) {
+    for (let x = 0; x < n; x++) {
+      if (s.length === 0) {
+        s = s + chars[x];
+        s = s + '1';
+      } //second last element of is same as curr element
+      else if (s[s.length - 2] === chars[x]) {
+        let count = Number(s[s.length - 1]) + 1;
+        s = s.substring(0, s.length - 1) + count;
+      } else {
+        s = s + chars[x];
+        s = s + '1';
+      }
     }
+  } else {
+    s = s + chars[0];
   }
-  return s.length;
-}
-console.log('Hello');
-stringCompression([]);
+  chars = s.split('');
+  return chars + ' ------> ' + chars.length;
+};
+
+console.log(compress(['a', 'a', 'b', 'b', 'b', 'c', 'c']));
+console.log(compress(['a']));
