3d70ebbd75ae1a267943a4c94974b4a1b62003d8
ghoshshreya <shrghosh08@gmail.com>
2023-03-02 16:12:23 +0530

    1 file modified

--- stringCompression.js
+++ stringCompression.js
@@ -1,24 +1,24 @@
 var compress = function (chars) {
-  let s = '';
-  let n = chars.length;
-  if (n > 0 && n > 1) {
-    for (let x = 0; x < n; x++) {
-      if (s.length === 0) {
-        s = s + chars[x];
-        s = s + '1';
-      } //second last element of is same as curr element
-      else if (s[s.length - 2] === chars[x]) {
-        let count = Number(s[s.length - 1]) + 1;
-        s = s.substring(0, s.length - 1) + count;
-      } else {
-        s = s + chars[x];
-        s = s + '1';
-      }
-    }
-  } else {
-    s = s + chars[0];
-  }
-  chars = s.split('');
+  // let s = '';
+  // let n = chars.length;
+  // if (n > 0 && n > 1) {
+  //   for (let x = 0; x < n; x++) {
+  //     if (s.length === 0) {
+  //       s = s + chars[x];
+  //       s = s + '1';
+  //     } //second last element of is same as curr element
+  //     else if (s[s.length - 2] === chars[x]) {
+  //       let count = Number(s[s.length - 1]) + 1;
+  //       s = s.substring(0, s.length - 1) + count;
+  //     } else {
+  //       s = s + chars[x];
+  //       s = s + '1';
+  //     }
+  //   }
+  // } else {
+  //   s = s + chars[0];
+  // }
+  // chars = s.split('');
   return chars + ' ------> ' + chars.length;
 };
 
